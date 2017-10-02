3ea4b38169932d340ed6bbebeaa62acfa84ecb79
John Daugherty <johndaugherty1986@gmail.com>
2017-10-01 20:14:51 -0500

    making nav

--- src/index.js
+++ src/index.js
@@ -6,3 +6,28 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+ReactDOM.render(<Router>
+  <div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/artists">Contact</Link></li>
+      <li><Link to="/releases">Contact</Link></li>
+
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route path="/contact" component={Contact} />
+    </ul>
+  </div>
+</Router>
+
+  , document.getElementById('root'));
+registerServiceWorker();
