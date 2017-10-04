a41c008a961ed63abe0e308009cec343dafbee27
John Daugherty <johndaugherty1986@gmail.com>
2017-10-03 22:26:39 -0500

    working on artists page

--- src/components/artists.js
+++ src/components/artists.js
@@ -3,9 +3,181 @@ import React, {Component } from 'react';
 class Artists extends Component {
   render(){
     return (
-      <div>
-        <h1> I am Artists </h1>
-      </div>
+         <div class="supporting">
+                  <div class="container">
+                    <h1 class="top"> Artists</h1>
+
+                      <div class="row">
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/dictateneuro"><img src="https://i1.sndcdn.com/avatars-000157652831-yi2bqm-t500x500.jpg"/></a>
+                              <h2>Dictate</h2>
+                              <p>37.0902° N, 95.7129° W</p>
+                            </div>
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/gemodubz"><img src="https://i1.sndcdn.com/avatars-000194217489-3oueca-t500x500.jpg"/></a>
+                              <h2>Gemo</h2>
+                              <p>53.4808° N, 2.2426° W</p>
+                            </div>
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/casual-hex"><img src="https://i1.sndcdn.com/avatars-000182867075-gz1nui-t500x500.jpg"/></a>
+                              <h2>Casual Hex</h2>
+                              <p>44.9778° N, 93.2650° W</p>
+                        </div>
+                      </div>
+
+                      <div class="row">
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/architekdnb"><img src="https://i1.sndcdn.com/avatars-000144643924-xx2jrw-t500x500.jpg"/></a>
+                              <h2>Architect</h2>
+                              <p>41.8781° N, 87.6298° W</p>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/bearhats"><img src="https://i1.sndcdn.com/avatars-000223501042-1s7c1h-t500x500.jpg"/></a>
+                              <h2>Bearhat</h2>
+                              <p>44.9778° N, 93.2650° W</p>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/benitsukidub"><img src="https://i1.sndcdn.com/avatars-000229452742-x4wr3z-t500x500.jpg"/></a>
+                              <h2>Benitsuki</h2>
+                              <p>44.9778° N, 93.2650° W</p>
+                          </div>
+                      </div>
+
+                      <div class="row">
+                          <div class="col-md-4">
+                            <a href-"https://soundcloud.com/iamchee"><img src="https://i1.sndcdn.com/avatars-000215968828-p1aqvl-t500x500.jpg"></a>
+                              <h2>Chee</h2>
+                              <p>25.7479° S, 28.2293° E</p>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/detmusic"><img src="https://i1.sndcdn.com/avatars-000075298844-k8mxsr-t500x500.jpg"></a>
+                              <h2>DET</h2>
+                              <p>40.4637° N, 3.7492° W</p>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/vscrl"><img src="https://i1.sndcdn.com/avatars-000226607260-vdtfdg-t500x500.jpg"/></a>
+                              <h2>Visceral</h2>
+                              <p>35.6225° N, 117.6709° W</p>
+                          </div>
+                      </div>
+
+
+        <!--- stuff !-->
+
+
+                      <div class="row">
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/DFNKTmusic"><img src="https://i1.sndcdn.com/avatars-000211253433-drzlhq-t500x500.jpg"/></a>
+                              <h2>DFNKT</h2>
+                              <p>37.0902° N, 95.7129° W</p>
+                            </div>
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/eyeseeu"><img src="https://i1.sndcdn.com/avatars-000199893931-csz6ek-t500x500.jpg"/></a>
+                              <h2>Eyeseeu</h2>
+                              <p>53.4808° N, 2.2426° W</p>
+                            </div>
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/copy-catt"><img src="https://i1.sndcdn.com/avatars-000224025162-0w939s-t500x500.jpg/"></a>
+                              <h2>Copycatt</h2>
+                              <p>44.9778° N, 93.2650° W</p>
+                        </div>
+                      </div>
+                      <div class="row">
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/jakespencerbass"><img src="https://i1.sndcdn.com/avatars-000227462316-ammdqv-t500x500.jpg"/></a>
+                              <h2>Jake Spencer</h2>
+                              <p>42.3314° N, 83.0458° W</p>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/grey-code"><img src="https://i1.sndcdn.com/avatars-000215505316-1p2a9y-t500x500.jpg"/></a>
+                              <h2>Greycode</h2>
+                              <p>44.9778° N, 93.2650° W</p>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/canopy_music"><img src="https://i1.sndcdn.com/avatars-000193528083-cp03th-t500x500.jpg"/></a>
+                              <h2>Canopy</h2>
+                              <p>44.9778° N, 93.2650° W</p>
+                          </div>
+                      </div>
+                      <div class="row">
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/nostalgia"><img src="https://i1.sndcdn.com/avatars-000153581390-h2712r-t500x500.jpg"></a>
+                              <h2>Nostalgia</h2>
+                              <p>25.7479° S, 28.2293° E</p>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/sinetwist"><img src="https://i1.sndcdn.com/avatars-000231354871-bjjlhs-t500x500.jpg"></a>
+                              <h2>Sine Twist</h2>
+                              <p>40.4637° N, 3.7492° W</p>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/vscrl"><img src="https://i1.sndcdn.com/avatars-000226607260-vdtfdg-t500x500.jpg"/></a>
+                              <h2>Visceral</h2>
+                              <p>35.6225° N, 117.6709° W</p>
+                          </div>
+                      </div>
+
+                      <div class="row">
+                          <div class="col-md-4">
+                            <a href-"https://soundcloud.com/fiejk"><img src="https://i1.sndcdn.com/avatars-000195746148-ztp5ya-t500x500.jpg"></a>
+                              <h2>Fijek</h2>
+                              <p>25.7479° S, 28.2293° E</p> <a href="#" class="btn">Go</a>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="3"><img src="https://i1.sndcdn.com/avatars-000195746148-ztp5ya-t500x500.jpg"></a>
+                              <h2>Kudo</h2>
+                              <p>40.4637° N, 3.7492° W</p>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="#"><img src="https://i1.sndcdn.com/avatars-000195746148-ztp5ya-t500x500.jpg"/></a>
+                              <h2>Kyra</h2>
+                              <p>35.6225° N, 117.6709° W</p>
+                          </div>
+                      </div>
+                      <div class="row">
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/fiejk"><img src="https://i1.sndcdn.com/avatars-000195746148-ztp5ya-t500x500.jpg"></a>
+                              <h2>PATHXS</h2>
+                              <p>25.7479° S, 28.2293° E</p> <a href="#" class="btn">Go</a>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="#"><img src="https://i1.sndcdn.com/avatars-000195746148-ztp5ya-t500x500.jpg"></a>
+                              <h2>Vyjil</h2>
+                              <p>40.4637° N, 3.7492° W</p>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="#"><img src="https://i1.sndcdn.com/avatars-000195746148-ztp5ya-t500x500.jpg"/></a>
+                              <h2>Uli</h2>
+                              <p>35.6225° N, 117.6709° W</p>
+                          </div>
+                      </div>
+                      <div class="row">
+                          <div class="col-md-4">
+                            <a href="https://soundcloud.com/critsyn"><img src="https://i1.sndcdn.com/avatars-000223796935-1sswxg-t500x500.jpg"></a>
+                              <h2>Critsyn</h2>
+                              <p>25.7479° S, 28.2293° E</p>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="#"><img src="https://i1.sndcdn.com/avatars-000195746148-ztp5ya-t500x500.jpg"></a>
+                              <h2>Yokaze</h2>
+                              <p>40.4637° N, 3.7492° W</p>
+                          </div>
+                          <div class="col-md-4">
+                            <a href="http://soundcloud.com/thinktwiceuk"><img src="https://i1.sndcdn.com/avatars-000231407804-f20bjv-t500x500.jpg"></a>
+                              <h2>Think Twice</h2>
+                              <p>40.4637° N, 3.7492° W</p>
+                          </div>
+                      </div>
+                      <div class="row">
+                          <div class="col-md-4">
+                            <a href-"https://soundcloud.com/thishollowearth"><img src="https://i1.sndcdn.com/avatars-000027919134-aejoze-t500x500.jpg"></a>
+                              <h2>Hollow Earth</h2>
+                              <p>25.7479° S, 28.2293° E</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
     );
   }
 }
