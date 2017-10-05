5df855d99e3c1adddf6253a534896d50ee8e5f82
John Daugherty <johndaugherty1986@gmail.com>
2017-10-04 23:47:56 -0500

    fixing nav

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Home from "./components/home";
 import Artists from './components/artists';
-import Contacts from './components/contacts';
+import Contact from './components/contact';
 import Releases from './components/releases';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -22,14 +24,14 @@ ReactDOM.render(<Router>
     <ul>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/contact">Contact</Link></li>
-      <li><Link to="/artists">Contact</Link></li>
-      <li><Link to="/releases">Contact</Link></li>
+      <li><Link to="/artists">Artists</Link></li>
+      <li><Link to="/releases">Releases</Link></li>
 
       <hr />
       <Route exact path="/" component={Home} />
       <Route path="/contact" component={Contact} />
-      <Route path="/artists" component={Contact} />
-      <Route path="/releases" component={Contact} />
+      <Route path="/artists" component={Artists} />
+      <Route path="/releases" component={Releases} />
 
     </ul>
   </div>
